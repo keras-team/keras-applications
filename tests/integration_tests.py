c4574290fc206da980cf1daf0f0467bd27b5309f
Taehoon Lee <me@taehoonlee.com>
2018-07-28 13:14:00 +0900

    Enable integration tests (#9)

--- tests/integration_tests.py
+++ tests/integration_tests.py
@@ -16,17 +16,13 @@ def test_that_internal_imports_are_not_overriden():
     if not hasattr(keras.applications, 'keras_applications'):
         return  # Old Keras, don't run.
 
-    # TODO: enable the following tests
-    # if get_source_inputs is included in tf.keras.utils
-    return
-
     import tensorflow as tf
     keras_applications.set_keras_submodules(
         backend=tf.keras.backend,
         layers=tf.keras.layers,
         models=tf.keras.models,
         utils=tf.keras.utils)
-    assert keras.applications.vgg16.backend is keras.backend
+    assert keras_applications._KERAS_BACKEND.__name__ == 'tensorflow.keras.backend'
 
     # Now test the reverse order
     del keras
@@ -39,7 +35,7 @@ def test_that_internal_imports_are_not_overriden():
         models=tf.keras.models,
         utils=tf.keras.utils)
     import keras
-    assert keras.applications.vgg16.backend is keras.backend
+    assert keras_applications._KERAS_BACKEND.__name__ == 'tensorflow.keras.backend'
 
 
 if __name__ == '__main__':
