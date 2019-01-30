8f353998c60ece5194b29e463b4a9a93778ec885
Francois Chollet <francois.chollet@gmail.com>
2019-01-29 16:47:16 -0800

    Update version number to 1.0.7.

--- setup.py
+++ setup.py
@@ -21,13 +21,13 @@ and is distributed under the MIT license.
 '''
 
 setup(name='Keras_Applications',
-      version='1.0.6',
+      version='1.0.7',
       description='Reference implementations of popular deep learning models',
       long_description=long_description,
       author='Keras Team',
       url='https://github.com/keras-team/keras-applications',
       download_url='https://github.com/keras-team/'
-                   'keras-applications/tarball/1.0.6',
+                   'keras-applications/tarball/1.0.7',
       license='MIT',
       install_requires=['numpy>=1.9.1',
                         'h5py'],
