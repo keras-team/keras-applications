ddc81304554f48e4ab73e8b82072a014423a903a
Taehoon Lee <me@taehoonlee.com>
2019-10-08 10:15:04 +0900

    Minor style fixes

--- keras_applications/efficientnet.py
+++ keras_applications/efficientnet.py
@@ -54,19 +54,19 @@ WEIGHTS_HASHES = {
 
 DEFAULT_BLOCKS_ARGS = [
     {'kernel_size': 3, 'repeats': 1, 'filters_in': 32, 'filters_out': 16,
-     'expand_ratio': 1, 'id_skip': True, 'strides': [1, 1], 'se_ratio': 0.25},
+     'expand_ratio': 1, 'id_skip': True, 'strides': 1, 'se_ratio': 0.25},
     {'kernel_size': 3, 'repeats': 2, 'filters_in': 16, 'filters_out': 24,
-     'expand_ratio': 6, 'id_skip': True, 'strides': [2, 2], 'se_ratio': 0.25},
+     'expand_ratio': 6, 'id_skip': True, 'strides': 2, 'se_ratio': 0.25},
     {'kernel_size': 5, 'repeats': 2, 'filters_in': 24, 'filters_out': 40,
-     'expand_ratio': 6, 'id_skip': True, 'strides': [2, 2], 'se_ratio': 0.25},
+     'expand_ratio': 6, 'id_skip': True, 'strides': 2, 'se_ratio': 0.25},
     {'kernel_size': 3, 'repeats': 3, 'filters_in': 40, 'filters_out': 80,
-     'expand_ratio': 6, 'id_skip': True, 'strides': [2, 2], 'se_ratio': 0.25},
+     'expand_ratio': 6, 'id_skip': True, 'strides': 2, 'se_ratio': 0.25},
     {'kernel_size': 5, 'repeats': 3, 'filters_in': 80, 'filters_out': 112,
-     'expand_ratio': 6, 'id_skip': True, 'strides': [1, 1], 'se_ratio': 0.25},
+     'expand_ratio': 6, 'id_skip': True, 'strides': 1, 'se_ratio': 0.25},
     {'kernel_size': 5, 'repeats': 4, 'filters_in': 112, 'filters_out': 192,
-     'expand_ratio': 6, 'id_skip': True, 'strides': [2, 2], 'se_ratio': 0.25},
+     'expand_ratio': 6, 'id_skip': True, 'strides': 2, 'se_ratio': 0.25},
     {'kernel_size': 3, 'repeats': 1, 'filters_in': 192, 'filters_out': 320,
-     'expand_ratio': 6, 'id_skip': True, 'strides': [1, 1], 'se_ratio': 0.25}
+     'expand_ratio': 6, 'id_skip': True, 'strides': 1, 'se_ratio': 0.25}
 ]
 
 CONV_KERNEL_INITIALIZER = {
@@ -115,41 +115,32 @@ def swish(x):
     return x * backend.sigmoid(x)
 
 
-def round_filters(filters, width_coefficient, depth_divisor):
-    """Round number of filters based on depth multiplier."""
-    filters *= width_coefficient
-    new_filters = int(filters + depth_divisor / 2) // depth_divisor * depth_divisor
-    new_filters = max(depth_divisor, new_filters)
-    # Make sure that round down does not go down by more than 10%.
-    if new_filters < 0.9 * filters:
-        new_filters += depth_divisor
-    return int(new_filters)
-
-
-def round_repeats(repeats, depth_coefficient):
-    """Round number of repeats based on depth multiplier."""
-    return int(math.ceil(depth_coefficient * repeats))
-
-
-def block(inputs, drop_rate=None, activation_fn=swish, name='', **kwargs):
+def block(inputs, activation_fn=swish, drop_rate=0., name='',
+          filters_in=32, filters_out=16, kernel_size=3, strides=1,
+          expand_ratio=1, se_ratio=0., id_skip=True):
     """A mobile inverted residual block.
 
     # Arguments
         inputs: input tensor.
-        args: dict, block arguments.
-        drop_rate: float between 0 and 1, fraction of the input units to drop.
         activation_fn: activation function.
+        drop_rate: float between 0 and 1, fraction of the input units to drop.
         name: string, block label.
+        filters_in: integer, the number of input filters.
+        filters_out: integer, the number of output filters.
+        kernel_size: integer, the dimension of the convolution window.
+        strides: integer, the stride of the convolution.
+        expand_ratio: integer, scaling coefficient for the input filters.
+        se_ratio: float between 0 and 1, fraction to squeeze the input filters.
+        id_skip: boolean.
 
     # Returns
         output tensor for the block.
     """
-    has_se = (kwargs['se_ratio'] is not None) and (0 < kwargs['se_ratio'] <= 1)
     bn_axis = 3 if backend.image_data_format() == 'channels_last' else 1
 
     # Expansion phase
-    filters = kwargs['filters_in'] * kwargs['expand_ratio']
-    if kwargs['expand_ratio'] != 1:
+    filters = filters_in * expand_ratio
+    if expand_ratio != 1:
         x = layers.Conv2D(filters, 1,
                           padding='same',
                           use_bias=False,
@@ -161,8 +152,8 @@ def block(inputs, drop_rate=None, activation_fn=swish, name='', **kwargs):
         x = inputs
 
     # Depthwise Convolution
-    x = layers.DepthwiseConv2D(kwargs['kernel_size'],
-                               strides=kwargs['strides'],
+    x = layers.DepthwiseConv2D(kernel_size,
+                               strides=strides,
                                padding='same',
                                use_bias=False,
                                depthwise_initializer=CONV_KERNEL_INITIALIZER,
@@ -171,40 +162,38 @@ def block(inputs, drop_rate=None, activation_fn=swish, name='', **kwargs):
     x = layers.Activation(activation_fn, name=name + 'activation')(x)
 
     # Squeeze and Excitation phase
-    if has_se:
-        reduced_filters = max(1, int(kwargs['filters_in'] * kwargs['se_ratio']))
-        se_tensor = layers.GlobalAveragePooling2D(name=name + 'se_squeeze')(x)
-        se_tensor = layers.Reshape((1, 1, filters),
-                                   name=name + 'se_reshape')(se_tensor)
-        se_tensor = layers.Conv2D(reduced_filters, 1,
-                                  activation=activation_fn,
-                                  padding='same',
-                                  kernel_initializer=CONV_KERNEL_INITIALIZER,
-                                  name=name + 'se_reduce')(se_tensor)
-        se_tensor = layers.Conv2D(filters, 1,
-                                  activation='sigmoid',
-                                  padding='same',
-                                  kernel_initializer=CONV_KERNEL_INITIALIZER,
-                                  name=name + 'se_expand')(se_tensor)
+    if 0 < se_ratio <= 1:
+        filters_se = max(1, int(filters_in * se_ratio))
+        se = layers.GlobalAveragePooling2D(name=name + 'se_squeeze')(x)
+        se = layers.Reshape((1, 1, filters), name=name + 'se_reshape')(se)
+        se = layers.Conv2D(filters_se, 1,
+                           padding='same',
+                           activation=activation_fn,
+                           kernel_initializer=CONV_KERNEL_INITIALIZER,
+                           name=name + 'se_reduce')(se)
+        se = layers.Conv2D(filters, 1,
+                           padding='same',
+                           activation='sigmoid',
+                           kernel_initializer=CONV_KERNEL_INITIALIZER,
+                           name=name + 'se_expand')(se)
         if backend.backend() == 'theano':
             # For the Theano backend, we have to explicitly make
             # the excitation weights broadcastable.
-            se_tensor = layers.Lambda(
+            se = layers.Lambda(
                 lambda x: backend.pattern_broadcast(x, [True, True, True, False]),
                 output_shape=lambda input_shape: input_shape,
-                name=name + 'se_broadcast')(se_tensor)
-        x = layers.multiply([x, se_tensor], name=name + 'se_excite')
+                name=name + 'se_broadcast')(se)
+        x = layers.multiply([x, se], name=name + 'se_excite')
 
     # Output phase
-    x = layers.Conv2D(kwargs['filters_out'], 1,
+    x = layers.Conv2D(filters_out, 1,
                       padding='same',
                       use_bias=False,
                       kernel_initializer=CONV_KERNEL_INITIALIZER,
                       name=name + 'project_conv')(x)
     x = layers.BatchNormalization(axis=bn_axis, name=name + 'project_bn')(x)
-    if (kwargs['id_skip'] and all(s == 1 for s in kwargs['strides']) and
-            kwargs['filters_in'] == kwargs['filters_out']):
-        if drop_rate and (drop_rate > 0):
+    if (id_skip is True and strides == 1 and filters_in == filters_out):
+        if drop_rate > 0:
             x = layers.Dropout(drop_rate,
                                noise_shape=(None, 1, 1, 1),
                                name=name + 'drop')(x)
@@ -238,11 +227,12 @@ def EfficientNet(width_coefficient,
     # Arguments
         width_coefficient: float, scaling coefficient for network width.
         depth_coefficient: float, scaling coefficient for network depth.
-        default_size: int, default input image size.
+        default_size: integer, default input image size.
         dropout_rate: float, dropout rate before final classifier layer.
         drop_connect_rate: float, dropout rate at skip connections.
-        depth_divisor: int.
-        blocks_args: A list of BlockArgs to construct block modules.
+        depth_divisor: integer, a unit of network width.
+        activation_fn: activation function.
+        blocks_args: list of dicts, parameters to construct block modules.
         model_name: string, model name.
         include_top: whether to include the fully-connected
             layer at the top of the network.
@@ -308,10 +298,23 @@ def EfficientNet(width_coefficient,
 
     bn_axis = 3 if backend.image_data_format() == 'channels_last' else 1
 
+    def round_filters(filters, divisor=depth_divisor):
+        """Round number of filters based on depth multiplier."""
+        filters *= width_coefficient
+        new_filters = max(divisor, int(filters + divisor / 2) // divisor * divisor)
+        # Make sure that round down does not go down by more than 10%.
+        if new_filters < 0.9 * filters:
+            new_filters += divisor
+        return int(new_filters)
+
+    def round_repeats(repeats):
+        """Round number of repeats based on depth multiplier."""
+        return int(math.ceil(depth_coefficient * repeats))
+
     # Build stem
     x = img_input
-    x = layers.Conv2D(round_filters(32, width_coefficient, depth_divisor), 3,
-                      strides=(2, 2),
+    x = layers.Conv2D(round_filters(32), 3,
+                      strides=2,
                       padding='same',
                       use_bias=False,
                       kernel_initializer=CONV_KERNEL_INITIALIZER,
@@ -323,35 +326,25 @@ def EfficientNet(width_coefficient,
     from copy import deepcopy
     blocks_args = deepcopy(blocks_args)
 
-    num_blocks_total = sum(block_args['repeats'] for block_args in blocks_args)
-    block_num = 0
-    for (idx, block_args) in enumerate(blocks_args):
-        assert block_args['repeats'] > 0
+    b = 0
+    blocks = float(sum(args['repeats'] for args in blocks_args))
+    for (i, args) in enumerate(blocks_args):
+        assert args['repeats'] > 0
         # Update block input and output filters based on depth multiplier.
-        block_args['filters_in'] = round_filters(
-            block_args['filters_in'], width_coefficient, depth_divisor)
-        block_args['filters_out'] = round_filters(
-            block_args['filters_out'], width_coefficient, depth_divisor)
-        block_args['repeats'] = round_repeats(
-            block_args['repeats'], depth_coefficient)
+        args['filters_in'] = round_filters(args['filters_in'])
+        args['filters_out'] = round_filters(args['filters_out'])
 
-        # The first block needs to take care of stride and filter size increase.
-        drop_rate = drop_connect_rate * float(block_num) / num_blocks_total
-        x = block(x, drop_rate, activation_fn,
-                  name='block{}a_'.format(idx + 1), **block_args)
-        block_num += 1
-        if block_args['repeats'] > 1:
-            block_args['filters_in'] = block_args['filters_out']
-            block_args['strides'] = [1, 1]
-            for bidx in range(block_args['repeats'] - 1):
-                drop_rate = drop_connect_rate * float(block_num) / num_blocks_total
-                x = block(x, drop_rate, activation_fn,
-                          name='block{}{}_'.format(idx + 1, chr(bidx + 98)),
-                          **block_args)
-                block_num += 1
+        for j in range(round_repeats(args.pop('repeats'))):
+            # The first block needs to take care of stride and filter size increase.
+            if j > 0:
+                args['strides'] = 1
+                args['filters_in'] = args['filters_out']
+            x = block(x, activation_fn, drop_connect_rate * b / blocks,
+                      name='block{}{}_'.format(i + 1, chr(j + 97)), **args)
+            b += 1
 
     # Build top
-    x = layers.Conv2D(round_filters(1280, width_coefficient, depth_divisor), 1,
+    x = layers.Conv2D(round_filters(1280), 1,
                       padding='same',
                       use_bias=False,
                       kernel_initializer=CONV_KERNEL_INITIALIZER,
@@ -360,7 +353,7 @@ def EfficientNet(width_coefficient,
     x = layers.Activation(activation_fn, name='top_activation')(x)
     if include_top:
         x = layers.GlobalAveragePooling2D(name='avg_pool')(x)
-        if dropout_rate and dropout_rate > 0:
+        if dropout_rate > 0:
             x = layers.Dropout(dropout_rate, name='top_dropout')(x)
         x = layers.Dense(classes,
                          activation='softmax',
